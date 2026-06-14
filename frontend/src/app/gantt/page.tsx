b3f2ac739aa7a5aaaf56194f995f92ceafbb9ba2
takutolow09@gmail.com <takutolow09@gmail.com>
2026-06-14 21:24:18 +0900

    ガントチャートの表示機能を改善

--- frontend/src/app/gantt/page.tsx
+++ frontend/src/app/gantt/page.tsx
@@ -7,22 +7,43 @@ const ganttTask = [
         title: "要件定義を確認する",
         start: 1,
         duration: 3,
+        status: "todo",
     },
     {
         id: 2,
         title: "テーブル定義を作成する",
         start: 4,
         duration: 4,
+        status: "doing",
     },
     {
         id: 3,
         title: "ER図を作成する",
         start: 8,
         duration: 2,
+        status: "review",
     },
+    {
+        id: 4,
+        title: "gitにpushする",
+        start: 4,
+        duration: 1,
+        status: "done",
+    }
 ];
 
 const days = ["6/1", "6/2", "6/3", "6/4", "6/5", "6/6", "6/7", "6/8", "6/9", "6/10"];
+
+const today = 5;
+
+const getBarColorClass = (status: string) => {
+    if(status === "todo") return "bg-zinc-400";
+    if(status === "doing") return "bg-blue-500";
+    if(status === "review") return "bg-yellow-400"
+    if(status === "done") return "bg-green-500";
+
+    return "bg-zinc-400";
+};
 
 const GanttPage = () => {
     return (
@@ -32,6 +53,27 @@ const GanttPage = () => {
                 <p className="mt-2 text-zinc-500">
                     タスクの開始日と期日日を横棒で確認できます。
                 </p>
+                <div className="mt-4 flex gap-4 text-sm">
+                    <div className="flex items-center gap-z">
+                        <div className="h-4 w-4 rounded bg-zinc-400" />
+                        <span>未対応</span>
+                    </div>
+
+                    <div className="flex items-center gap-2">
+                        <div className="h-4 w-4 rounded bg-blue-400" />
+                        <span>対応中</span>
+                    </div>
+
+                    <div className="flex items-center gap-2">
+                        <div className="h-4 w-4 rounded bg-yellow-400" />
+                        <span>確認中</span>
+                    </div>
+
+                    <div className="flex items-center gap-2">
+                        <div className="h-4 w-4 rounded bg-green-500" />
+                        <span>対応済み</span>
+                    </div>
+                </div>
             </div>
 
             <div className="rounded-xl border bg-white p-4 shadow-sm">
@@ -40,14 +82,22 @@ const GanttPage = () => {
                         タスク名
                     </div>
 
-                    {days.map((day) => (
-                        <div
-                         kay={day}
-                         className="border-b border-r bg-zinc-50 p-3 text-center font-bold"
-                        >
-                            {day}
-                        </div>
-                    ))}
+                    {days.map((day, index) => {
+                        const currentDay = index + 1;
+
+                        return (
+                            <div
+                             key={day}
+                             className={`border-b border-r p-3 text-center font-bold ${
+                                currentDay === today
+                                 ? "bg-red-100 text-red-600"
+                                 : "bg-zinc-50"
+                             }`}
+                             >
+                                {day}
+                            </div>
+                        )
+                    })}
 
                     {ganttTask.map((task) => (
                         <Fragment key={task.id}>
@@ -67,10 +117,12 @@ const GanttPage = () => {
                         return (
                             <div
                              key={`${task.id}-${day}`}
-                             className="border-b border-r p-2"
+                             className={`border-b border-r p-2 ${
+                                currentDay === today ? "bg-red-50" : ""
+                             }`}
                             >
                                 {isInRange && (
-                                    <div className="h-6 rounded bg-blue-500" />
+                                    <div className={`h-6 rounded ${getBarColorClass(task.status)}`} />
                                 )}
                             </div>
                         )
